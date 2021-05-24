975fa40a7c6cbf17109e38613be86491b4865936
Albin Kjellin <albin.kjellin@gmail.com>
2021-05-24 12:52:05 +0200

    Added script for time partitioning.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # soda-scripting
+
+## Analyze and Scan all tables in the database
+
+This script will automate analyzing and scanning all tables in a database. This can be triggered via jenkins or any other orchestrator. The idea is to get up and running quickly with a few basic tests.
+
+Triggger by calling:
+
+```
+sh ../analyze-scan-all-tables.sh
+```
+
+## Run a scan based on a date partition
+This script will run scans for each date between a start date and an enddate for each data in that range a scan will be triggered with a filter that will on that date. Then the result is sent to Soda Cloud in a way that it looks like the scan was executed on that day.
---
+++ run_scan_per_date.sh
@@ -0,0 +1,23 @@
+#Provide a valid startdate
+startdate=2021-05-20
+#Provide a valid startdate
+enddate=2021-05-23
+
+sDateTs=`date -j -f "%Y-%m-%d" $startdate "+%s"`
+eDateTs=`date -j -f "%Y-%m-%d" $enddate "+%s"`
+dateTs=$sDateTs
+offset=86400
+
+while [ "$dateTs" -le "$eDateTs" ]
+do
+  date=`date -j -f "%s" $dateTs "+%Y-%m-%d"`
+  #Scantime time is hardcoded so it will not generate duplicates in Soda Cloud.
+  scantimestamp="${date}T09:00:00+02:00"
+  printf '%s\n' $scantimestamp
+  printf '%s\n' $date
+  # -v option is for the filter in the scan yaml file.
+  # -t option is to set the scan time so that historical scans can be
+  # logged in Soda Cloud.
+  soda scan postgres-local/warehouse.yml postgres-local/tables/entaincustomer.yml -v date=$date -t $scantimestamp
+  dateTs=$(($dateTs+$offset))
+done
